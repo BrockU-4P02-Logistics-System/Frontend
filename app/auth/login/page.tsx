d4370036a9a6512f917b19f69ed110448bbe7f77
Tristan Arana <aranachildren@gmail.com>
2025-04-13 19:18:07 -0400

    redirect to dash while logged in

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react';
+import React, { useEffect } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { Button } from "@/components/ui/button";
@@ -15,7 +15,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { toast } from "sonner";
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import Image from "next/image"
 
 const { SITE_URL } = process.env;
@@ -29,6 +29,15 @@ export default function LoginPage() {
   const [isLoading, setIsLoading] = React.useState(false);
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
+
+  const { status } = useSession();
+  
+
+   useEffect(() => {
+      if (status === "authenticated") {
+        router.push("/dashboard");
+      }
+    }, [status, router]);
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
