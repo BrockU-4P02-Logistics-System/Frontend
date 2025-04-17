ec0d13869a64cc3eec45f511e0f817a6200c832f
ccorbett0116 <100178889+ccorbett0116@users.noreply.github.com>
2025-04-17 00:55:15 -0400

    increased packet size for route saving

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        serverActions: {
+            bodySizeLimit: '50mb', // Adjust the limit as needed
+        },
+    },
+};
 
 export default nextConfig;
