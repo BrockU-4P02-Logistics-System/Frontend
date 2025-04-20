43f2d6829036f68afe0798081a725ee016f236b9
Jordan <jordan@mynameisjordan.com>
2025-04-20 11:18:02 -0400

    Added default user icon to dashboard bottom left panel

--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -1,15 +1,8 @@
-"use client"
+"use client";
 
-import {
-  ChevronsUpDown,
-  LogOut,
-} from "lucide-react"
+import { ChevronsUpDown, LogOut, User } from "lucide-react";
 
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from "@/components/ui/avatar"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -17,32 +10,30 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from "@/components/ui/dropdown-menu";
 import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
   useSidebar,
-} from "@/components/ui/sidebar"
+} from "@/components/ui/sidebar";
 
 import { signOut } from "next-auth/react";
-import { Button } from "./ui/button"
+import { Button } from "./ui/button";
 
 export function NavUser({
   user,
 }: {
   user: {
-    name: string
-    email: string
-    avatar: string
-  }
+    name: string;
+    email: string;
+    avatar: string;
+  };
 }) {
-  const { isMobile } = useSidebar()
+  const { isMobile } = useSidebar();
 
   const handleLogout = async () => {
-
-	  await signOut({ callbackUrl: "/"});
-
+    await signOut({ callbackUrl: "/" });
   };
 
   return (
@@ -71,29 +62,31 @@ export function NavUser({
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
                   <AvatarImage src={user.avatar} alt={user.name} />
-                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                  <AvatarFallback className="rounded-lg">
+                    <User className="h-4 w-4" />
+                  </AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                   <span className="truncate font-semibold">{user.name}</span>
-	           <span className="truncate text-xs">{user.email}</span>
+                  <span className="truncate font-semibold">{user.name}</span>
+                  <span className="truncate text-xs">{user.email}</span>
                 </div>
               </div>
             </DropdownMenuLabel>
             <DropdownMenuSeparator />
-           
+
             <DropdownMenuItem>
-		    <Button
-		      variant="outline"
-		      size="icon"
-		      onClick={handleLogout} //function for handling click
-		    >
-		      <LogOut className="h-4 w-4" />
-		    </Button>
-        Logout
+              <Button
+                variant="outline"
+                size="icon"
+                onClick={handleLogout} //function for handling click
+              >
+                <LogOut className="h-4 w-4" />
+              </Button>
+              Logout
             </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
       </SidebarMenuItem>
     </SidebarMenu>
-  )
+  );
 }
