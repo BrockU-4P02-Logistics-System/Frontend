c5d0e5e6a881e91a7af89cece08d40edc1b2b608
Cameron Carvalho <57427798+cjcameron92@users.noreply.github.com>
2025-04-27 20:56:40 -0400

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,5 @@
 export const CURRENCY = "usd";
-// Set your amount limits: Use float for decimal currencies and
+// Set your amount limits: Use float for decimal currencies and2
 // Integer for zero-decimal currencies: https://stripe.com/docs/currencies#zero-decimal.
 export const MIN_AMOUNT = 10.0;
 export const MAX_AMOUNT = 5000.0;
